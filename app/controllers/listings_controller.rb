e9888041090c70a3cde784f32c74b45ec1af0f0b
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-05-15 10:47:12 +1000

    not working state - working on forms

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,6 @@
 class ListingsController < ApplicationController
+
+
   def index
     @listings = Listing.find(params[:id])
   end
@@ -7,8 +9,22 @@ class ListingsController < ApplicationController
   end
 
   def new
+    @game = Game.find(params[:id])
+    @listing = Listing.new
+  end
+
+  def create
+    @game = Game.find(params[:id])
+    puts listing_params
+    # newListing = Listing.create(game_id: @game.id, user_id: current_user.id, title: )
   end
 
   def update
+  end
+
+  private
+  
+  def listing_params
+    params.require(:listing).permit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'main#index'
-  get '/listings/:id', to: 'listings#index'
+  get '/listings/:id', to: 'listings#index', as: 'listings'
+  get '/listings/:id/new', to: 'listings#new'
+  post '/listings/:id/new', to: 'listings#create'
 end
