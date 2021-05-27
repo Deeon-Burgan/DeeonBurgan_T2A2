023ed2a791edd60026c3133d4a47062e2e0d168c
Deeon Burgan <deeonburgan@gmail.com>
2021-05-27 21:07:16 +1000

    changes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,7 @@ Game.destroy_all
 Listing.destroy_all
 Bid.destroy_all
 
-runescape = Game.create(name: "RuneScape", description: "Best game ever made")
-user1 = User.create(email: 'deeon@outlook.com', password: 'password')
-listing1 = runescape.listings.create(title: 'SGS', description: 'big sword for 2m', status: 0, user_id: user1.id)
-bid = listing1.bids.create(user_id: user1.id, listing_id: listing1.id, title: "2m for it", description: "i'll give ya 2m mate", status: 0)
-
-worldofwarcraft = Game.create(name: "World of Warcraft", description: "Best MMORPG ever made, honestly")
-user2 = User.create(email: 'donald@outlook.com', password: 'password')
-listing2 = runescape.listings.create(title: 'Shard of Azzinoth', description: 'one half of a whole', status: 0, user_id: user2.id)
-bid = listing2.bids.create(user_id: user2.id, listing_id: listing2.id, title: "Free?", description: "I'll give ya a high five", status: 0)
+user1 = User.create(email: 'dburgan@live.com', password: 'password', admin: true)
+runescape = Game.create(name: "RuneScape", description: "MMORPG")
+worldofwarcraft = Game.create(name: "World of Warcraft", description: "MMORPG")
 
